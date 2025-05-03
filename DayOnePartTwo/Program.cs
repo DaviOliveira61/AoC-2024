61e3a81a5e904a15471e599383835a6cd47beef3
davigbso@hotmail.com <davigbso@hotmail.com>
2025-05-02 23:24:27 -0300

    Add day two part two and resolve it

---
+++ DayOnePartTwo/Program.cs
@@ -0,0 +1,47 @@
+﻿var text = "";
+using (var file = new StreamReader("./input.txt"))
+{
+    text = file.ReadToEnd();
+}
+
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+var lines = text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var line in lines)
+{
+    var parts = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length == 2 &&
+        int.TryParse(parts[0], out int left) &&
+        int.TryParse(parts[1], out int right))
+    {
+        leftList.Add(left);
+        rightList.Add(right);
+    }
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var similarity = 0;
+for (int i = 0; i < leftList.Count; i++)
+{
+    var numberInRight = 0;
+    Console.WriteLine($"Left number {leftList[i]}");
+    for (int j = 0; j < rightList.Count; j++)
+    {
+
+        Console.WriteLine($"right number {rightList[j]}");
+        if (leftList[i] == rightList[j])
+        {
+
+            numberInRight++;
+        }
+    }
+    Console.WriteLine($"number in right: {numberInRight}");
+    similarity += numberInRight * leftList[i];
+}
+
+Console.WriteLine($"result: {similarity}");
