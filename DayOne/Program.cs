2d1982308f9c46d6946ca3356942aa76706e8171
davigbso@hotmail.com <davigbso@hotmail.com>
2025-05-02 22:13:50 -0300

    Day One completed

--- DayOne/Program.cs
+++ DayOne/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿var text = "";
+using (var file = new StreamReader("./input.txt"))
+{
+    text = file.ReadToEnd();
+}
+
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+var lines = text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var line in lines)
+{
+    var parts = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length == 2 &&
+        int.TryParse(parts[0], out int left) &&
+        int.TryParse(parts[1], out int right))
+    {
+        leftList.Add(left);
+        rightList.Add(right);
+    }
+}
+
+leftList.Sort();
+rightList.Sort();
+
+int totalDistance = 0;
+for (int i = 0; i < leftList.Count; i++)
+{
+    totalDistance += Math.Abs(leftList[i] - rightList[i]);
+}
+
+Console.WriteLine(totalDistance);
